8fe5bc3aa907d213a77b0e8817505c975d2fe20a
dis-zephyro <zephyro@yandex.ru>
2015-09-28 15:50:28 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 $(".popup").fancybox({
     "padding" : 0
 });
+
+
+// Подключние Яндекс-Карты
+
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+        center: [55.7499,37.6290], // Москва
+        zoom: 16,
+        controls: []
+    });
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: ''
+    }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'img/placemark.png',
+        // Размеры метки.
+        iconImageSize: [37, 48],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-18, -50]
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+    myMap.geoObjects.add(myPlacemark);
+}
